b093c341fd172d49a870d9d34adda296b111bb59
Alim Nastaev <a.nastaev@gmail.com>
2020-08-28 12:39:49 -0400

    some updates and fix for checkbox

--- lib/live_view_todos_web/live/todo_live.ex
+++ lib/live_view_todos_web/live/todo_live.ex
@@ -4,23 +4,37 @@ defmodule LiveViewTodosWeb.TodoLive do
   alias LiveViewTodos.Todos
   alias LiveViewTodosWeb.TodoView
 
+  # LiveView is a module with 3 callback functions:
+  #        1. mount()
+  #        2. handle_event()
+  #        3. render()
+
+  # assigns the initial state of the LiveView process
   def mount(_session, socket) do
     {:ok, fetch(socket)}
   end
 
-  def render(assigns) do
-    TodoView.render("todos.html", assigns)
-  end
-
+  # changes the state of the process
   def handle_event("add", %{"todo" => todo}, socket) do
     Todos.create_todo(todo)
 
     {:noreply, fetch(socket)}
   end
 
+  # changes the state of the process
+  def handle_event("toggle_done", id, socket) do
+    todo = Todos.get_todo!(id)
+
+    Todos.update_todo(todo, %{done: !todo.done})
+    {:noreply, fetch(socket)}
+  end
+
+  # renders a new view for the newly updated state
+  def render(assigns) do
+    TodoView.render("todos.html", assigns)
+  end
+
   defp fetch(socket) do
     assign(socket, todos: Todos.list_todos())
   end
 end
-
-
